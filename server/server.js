12cadd16246da461f95b0896d550333b7f1885a6
Jarosław Leśniak <jarek9244@gmail.com>
2020-02-17 12:02:01 +0100

    Added key sender event

--- server/server.js
+++ server/server.js
@@ -1,12 +1,10 @@
 const express = require('express');
-const keySender = require('node-key-sender');
-const robot = require('robotjs');
+const keySender = require('robotjs');
 
 const app = express();
 
 app.get('/key/:key', (req, res, next) => {
-    keySender.sendKey('@142', 2000);
-    keySender.s
+    robot.keyTap(req.params.key);
     next();
 });
 
